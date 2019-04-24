1a0b743d25cbc4ff1677514d1d3b0612ec2631c7
Timothy Shores <timothymshores@gmail.com>
2019-04-24 18:03:52 -0500

    Create POST route handler to /api/users/

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -27,4 +27,22 @@ usersRouter.get('/:id', (req, res) => {
         .catch(err => { res.status(500).json(message500) })
 });
 
+usersRouter.post('/', (req, res) => {
+    const { name } = req.body;
+    const message400 = { error: "Please provide a name for the user." }
+    const message500 = { error: "There was an error while saving the user to the database" };
+
+    console.log('name ', name);
+
+    if (name === '') {
+        res.status(400).json(message400);
+    }
+    else {
+        Users
+            .insert({ name })
+            .then(user => { res.status(201).json(user) })
+            .catch(err => { res.status(500).json(message500) })
+    }
+});
+
 module.exports = usersRouter;
