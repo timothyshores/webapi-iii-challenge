3c4655426fb40699886214eec20d1b9672fd17d8
Timothy Shores <timothymshores@gmail.com>
2019-04-24 17:47:04 -0500

    Create GET route handler to /api/users/

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -2,4 +2,15 @@ const express = require('express');
 const Users = require('./userDb');
 const usersRouter = express.Router();
 
+usersRouter.get('/', (req, res) => {
+    const message500 = { error: "The users information could not be retrieved." };
+
+    Users
+        .get()
+        .then(users => { res.status(200).json(users) })
+        .catch(err => {
+            res.status(500).json(message500);
+        })
+});
+
 module.exports = usersRouter;
