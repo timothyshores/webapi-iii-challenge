48795ce9cc9bfd37ea06a59cfdb52dc931abc70e
Timothy Shores <timothymshores@gmail.com>
2019-04-24 16:40:06 -0500

    Create GET route handler to /api/posts/id

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -2,14 +2,28 @@ const express = require('express');
 const Posts = require('./postDb');
 const postsRouter = express.Router();
 
+const message500 = { error: "The posts information could not be retrieved." };
+
 postsRouter.get('/', (req, res) => {
     Posts
         .get()
-        .then(users => { res.status(200).json(users) })
+        .then(posts => { res.status(200).json(posts) })
         .catch(err => {
-            const message500 = { error: "The users information could not be retrieved." };
             res.status(500).json(message500);
         })
 });
+
+postsRouter.get('/:id', (req, res) => {
+    const message404 = { error: "The post with the specified ID does not exist." }
+    const message500 = { error: "The post information could not be retrieved." }
+
+    Posts
+        .getById(req.params.id)
+        .then(post => {
+            post ? res.status(200).json(post) : res.status(404).json(message404);
+        })
+        .catch(err => { res.status(500).json(message500) })
+});
+
 
 module.exports = postsRouter;
