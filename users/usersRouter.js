6c042f5f9ccec3d8cef10950f709841ffc2148e1
Timothy Shores <timothymshores@gmail.com>
2019-04-24 17:58:19 -0500

    Create GET route handler to /api/users/:id

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -13,4 +13,18 @@ usersRouter.get('/', (req, res) => {
         })
 });
 
+usersRouter.get('/:id', (req, res) => {
+    const message404 = { error: "The user with the specified ID does not exist." }
+    const message500 = { error: "The user information could not be retrieved." }
+
+    Users
+        .getById(req.params.id)
+        .then(user => {
+            user
+                ? res.status(200).json(user)
+                : res.status(404).json(message404);
+        })
+        .catch(err => { res.status(500).json(message500) })
+});
+
 module.exports = usersRouter;
